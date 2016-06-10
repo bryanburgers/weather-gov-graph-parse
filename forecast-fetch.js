317ac1c1b0f110e952154caf4b61129ffe973d48
Bryan Burgers <bryan@burgers.io>
2016-06-10 06:32:06 -0500

    Send user-agent
    A 403 is returned if no user agent is sent when requesting data.

--- forecast-fetch.js
+++ forecast-fetch.js
@@ -7,7 +7,13 @@ module.exports = function fetch(latitude, longitude, hoursAhead) {
 	var deferred = Q.defer();
 
 	var url = "http://forecast.weather.gov/MapClick.php?w0=t&w1=td&w2=hi&w3=sfcwind&w3u=1&w4=sky&w5=pop&w6=rh&AheadHour=" + hoursAhead + "&FcstType=digital&textField1=" + latitude + "&textField2=" + longitude;
-	request(url, function(error, response, body) {
+	request({
+		url: url,
+		headers: {
+			'User-Agent': 'WeatherGraph/1.0',
+			'Accept': 'text/html'
+		}
+	}, function(error, response, body) {
 		if (error) {
 			deferred.reject(new Error(error));
 		}
